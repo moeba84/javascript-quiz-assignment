12155ee8f7894ac72493d4d5a4476a8d87618fe2
moeba84 <119361569+moeba84@users.noreply.github.com>
2023-01-23 15:10:26 -0800

    renamed style.css and script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+var questionsEl=document.querySelector("questionsbox")
+var currentquestionIndex= 0
+var questions=[
+    {
+        title: "what is not a primitive type in javascrript?",
+        choices: ["String", "Text", "Boolean", "Number"],
+        answer: "Text"
+    },{
+        title: "Which symbol represents strict equality in javascript?",
+        choices: ["=","!==","==","==="],
+        answer: "==="
+    }
+]
+questionsEl.innerHTML=questions[0]
+function getQuestion(){
+    var currentQuestion= questions[
+         currentquestionIndex
+    ]
+    var titleEl= document.getelementbyid("questiontitle")
+    titleEl.textContent= currentQuestion.title
+    for(var i=0;i<currentQuestion.choices.length; i++)
+    var choice = currentQuestion.choices[i]
+    
+
+}
